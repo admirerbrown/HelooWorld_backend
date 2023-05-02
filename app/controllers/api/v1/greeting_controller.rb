266dc4b0bcd57d2287c0c2f7866635c42f5c0fca
admirerbrown <admirerbrown@gmail.com>
2023-05-02 23:09:07 +0000

    Update server to allow cors

--- app/controllers/api/v1/greeting_controller.rb
+++ app/controllers/api/v1/greeting_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GreetingController < ApplicationController
+  before_action :set_cors_headers
+
   def index
     @greetings = Greeting.all
     if @greetings.empty?
@@ -7,5 +9,13 @@ class Api::V1::GreetingController < ApplicationController
       @greet = @greetings.sample
       render json: @greet
     end
+  end
+
+  private
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*' # Replace with the domain or origin of your app
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE' # Specify the allowed HTTP methods
+    headers['Access-Control-Allow-Headers'] = 'Content-Type' # Specify the allowed request headers
   end
 end
